32504a35220e199abd959eca453ada12b52695f6
Eric Le <eric4leformal@gmail.com>
2018-10-30 12:30:51 -0500

    added

---
+++ Code/Yahtzee/Assets/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Game{
+
+    public void throwDice()
+    {
+        //todo
+    }
+
+    public void saveDice()
+    {
+        //todo
+    }
+
+    public void score(Player player, ScoreSheet scoreSheet)
+    {
+        //todo
+    }
+
+    public Player endGame(Player player)
+    {
+        //todo
+    }
+}
---
+++ Code/Yahtzee/Assets/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class SettingsScript{
+
+    void setVolume() {
+        //Todo
+    }
+
+    void setSong()
+    {
+        //Todo
+    }
+
+    void setLanguage()
+    {
+        //todo
+    }
+}
